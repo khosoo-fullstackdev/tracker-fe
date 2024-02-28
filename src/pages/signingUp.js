0218615c9e56323ccf67f8a645c3155858281b95
Angi <khsoo.fsdev@gmail.com>
2024-02-28 15:22:37 +0800

    asd

--- src/pages/signingUp.js
+++
@@ -1,9 +0,0 @@
-import SigningUp from "@/components/sign-up/SigningUp";
-
-export default function signUp() {
-  return (
-    <div>
-      <SigningUp />
-    </div>
-  );
-}
--- src/pages/signingUp/currency.jsx
+++ src/pages/signingUp/currency.jsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 
-export default function SigningUp() {
+export default function currency() {
   const router = useRouter();
   return (
     <div className="w-screen h-screen m-auto flex flex-col pt-10 bg-white gap-[141px] ">
@@ -47,8 +47,8 @@ export default function SigningUp() {
           </svg>
         </div>
         <ul data-theme="light" className="steps">
-          <li className="step">Currency</li>
-          <li className="step">Balance</li>
+          <li className="step step-primary">Currency</li>
+          <li className="step step-primary">Balance</li>
           <li className="step">Finish</li>
         </ul>
       </div>
@@ -62,10 +62,10 @@ export default function SigningUp() {
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <g id="Money">
+              <g id="Coins">
                 <path
                   id="Vector"
-                  d="M21 16C21 16.9889 20.7068 17.9556 20.1573 18.7779C19.6079 19.6001 18.827 20.241 17.9134 20.6194C16.9998 20.9978 15.9945 21.0969 15.0245 20.9039C14.0546 20.711 13.1637 20.2348 12.4645 19.5355C11.7652 18.8363 11.289 17.9454 11.0961 16.9755C10.9031 16.0055 11.0022 15.0002 11.3806 14.0866C11.759 13.173 12.3999 12.3921 13.2221 11.8427C14.0444 11.2932 15.0111 11 16 11C17.3261 11 18.5979 11.5268 19.5355 12.4645C20.4732 13.4021 21 14.6739 21 16ZM31 8V24C31 24.2652 30.8946 24.5196 30.7071 24.7071C30.5196 24.8946 30.2652 25 30 25H2C1.73478 25 1.48043 24.8946 1.29289 24.7071C1.10536 24.5196 1 24.2652 1 24V8C1 7.73478 1.10536 7.48043 1.29289 7.29289C1.48043 7.10536 1.73478 7 2 7H30C30.2652 7 30.5196 7.10536 30.7071 7.29289C30.8946 7.48043 31 7.73478 31 8ZM29 13.7937C27.8645 13.458 26.8311 12.8435 25.9938 12.0062C25.1565 11.1689 24.542 10.1355 24.2062 9H7.79375C7.45801 10.1355 6.84351 11.1689 6.00623 12.0062C5.16895 12.8435 4.1355 13.458 3 13.7937V18.2062C4.1355 18.542 5.16895 19.1565 6.00623 19.9938C6.84351 20.8311 7.45801 21.8645 7.79375 23H24.2062C24.542 21.8645 25.1565 20.8311 25.9938 19.9938C26.8311 19.1565 27.8645 18.542 29 18.2062V13.7937Z"
+                  d="M23 11.1962V10.5C23 7.365 18.2712 5 12 5C5.72875 5 1 7.365 1 10.5V15.5C1 18.1112 4.28125 20.1863 9 20.8075V21.5C9 24.635 13.7288 27 20 27C26.2712 27 31 24.635 31 21.5V16.5C31 13.9125 27.8225 11.835 23 11.1962ZM7 18.3587C4.55125 17.675 3 16.5487 3 15.5V13.7413C4.02 14.4637 5.38625 15.0463 7 15.4375V18.3587ZM17 15.4375C18.6138 15.0463 19.98 14.4637 21 13.7413V15.5C21 16.5487 19.4487 17.675 17 18.3587V15.4375ZM15 24.3587C12.5513 23.675 11 22.5487 11 21.5V20.9788C11.3287 20.9913 11.6613 21 12 21C12.485 21 12.9587 20.9837 13.4237 20.9562C13.9403 21.1412 14.4665 21.2981 15 21.4263V24.3587ZM15 18.7812C14.0068 18.928 13.004 19.0011 12 19C10.996 19.0011 9.99324 18.928 9 18.7812V15.8075C9.99472 15.9371 10.9969 16.0014 12 16C13.0031 16.0014 14.0053 15.9371 15 15.8075V18.7812ZM23 24.7812C21.0106 25.0729 18.9894 25.0729 17 24.7812V21.8C17.9944 21.9337 18.9967 22.0005 20 22C21.0031 22.0014 22.0053 21.9371 23 21.8075V24.7812ZM29 21.5C29 22.5487 27.4487 23.675 25 24.3587V21.4375C26.6138 21.0462 27.98 20.4637 29 19.7412V21.5Z"
                   fill="white"
                 />
               </g>
@@ -74,24 +74,19 @@ export default function SigningUp() {
         </div>
 
         <p className="text-slate-900 text-2xl font-semibold leading-loose pb-6">
-          Select base currency
+          Set up your cash Balance
         </p>
-        <select className="w-96 h-16 p-4 bg-gray-100 text-gray-800 rounded-lg border border-gray-300">
-          <option className="text-gray-800 font-semibold leading-normal">
-            MNT - Mongolian Tugrik
-          </option>
-          <option className="text-gray-800 font-semibold leading-normal">
-            EUR - European Euro
-          </option>
-          <option className="text-gray-800 font-semibold leading-normal">
-            USD - American Dollar
-          </option>
-        </select>
+        <input
+          value="Wallet"
+          className="flex w-96 h-12 p-4 bg-gray-100 rounded-lg border border-gray-300 justify-start items-center text-neutral-400 text-base font-normal leading-normal"
+        />
         <p className="w-96 text-slate-600 pt-[12px] text-xs font-normal leading-none pb-8">
-          Your base currency should be the one you use most often. All
-          transaction in other currencies will be calculated based on this one{" "}
+          How much cash do you have in your wallet?
         </p>
-        <button className="flex w-96 h-12 px-4 bg-blue-600 rounded-[20px] justify-center items-center text-white text-xl font-normal leading-7">
+        <button
+          onClick={() => router.push("/signingUp/finish")}
+          className="flex w-96 h-12 px-4 bg-blue-600 rounded-[20px] justify-center items-center text-white text-xl font-normal leading-7"
+        >
           Confirm
         </button>
       </div>
