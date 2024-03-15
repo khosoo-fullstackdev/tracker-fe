71e7f8db09ba956f6166f140153f906517d4b68f
khosoo.fullstackdev <khosoo.fsdev@gmail.com>
2024-03-15 12:34:52 +0800

    asd

--- src/pages/record.js
+++ src/pages/record.js
@@ -1,8 +1,26 @@
 import Header from "@/components/Header";
 
 import RecordsBar from "@/components/record/RecordsBar";
+import { useState } from "react";
 
 export default function Record() {
+  const BE_URL = "http://localhost:4000/getCategory";
+  const [category, setCategory] = useState();
+  const handleCategory = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(),
+    };
+    const FETCHED_DATA = await fetch(BE_URL, options);
+    const FETCHED_JSON = await FETCHED_DATA.json();
+    console.log(FETCHED_JSON);
+
+    // setCategory(FETCHED_JSON.name);
+  };
+  // handleCategory();
   return (
     <div className="w-screen flex flex-col m-auto">
       <Header />
